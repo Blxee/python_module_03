44fdbe3c78f88496bf428623648c857dc8f7bb40
Ubuntu <aminedev57@gmail.com>
2025-12-14 10:46:19 +0000

    Added: day 03 of the python piscine

--- ex3/ft_achievement_tracker.py
+++ ex3/ft_achievement_tracker.py
@@ -1,9 +1,15 @@
 if __name__ == "__main__":
     print("=== Achievement Tracker System ===\n")
 
-    alice = {'first_kill', 'level_10', 'treasure_hunter', 'speed_demon'}
-    bob = {'first_kill', 'level_10', 'boss_slayer', 'collector'}
-    charlie = {'level_10', 'treasure_hunter', 'boss_slayer', 'speed_demon', 'perfectionist'}
+    alice = {"first_kill", "level_10", "treasure_hunter", "speed_demon"}
+    bob = {"first_kill", "level_10", "boss_slayer", "collector"}
+    charlie = {
+        "level_10",
+        "treasure_hunter",
+        "boss_slayer",
+        "speed_demon",
+        "perfectionist",
+    }
 
     print("Player alice achievements:", alice)
     print("Player bob achievements:", bob)
--- ex4/ft_inventory_system.py
+++ ex4/ft_inventory_system.py
@@ -1,5 +1,89 @@
-if __name__ = "__main__":
+if __name__ == "__main__":
+
+    def print_inventory(inventory: dict) -> None:
+        value = 0
+        count = 0
+        categories = {}
+        for key, item in inventory.items():
+            if key == "username":
+                continue
+
+            print(
+                f"{key} ({item['type']}, {item['rarity']}): {item['amount']}x @ {item['price']} gold each = {item['amount'] * item['price']} gold"
+            )
+
+            value += item["price"] * item["amount"]
+            count += item["amount"]
+            if item["type"] in categories:
+                categories[item["type"]] += item["amount"]
+            else:
+                categories[item["type"]] = item["amount"]
+
+        print(f"\nInventory value: {value} gold")
+        print(f"Item count: {count} items")
+        print(f"Categories: ", end="")
+
+        for i, (type, count) in enumerate(categories.items()):
+            print(f"{type}({count})", end="")
+            if i == len(categories) - 1:
+                print()
+            else:
+                print(", ", end="")
+
+    def trade_items(inv1: dict, inv2: dict, item: str, amount: int) -> None:
+        print(
+            f"=== Transaction: {inv1['username']} gives {inv2['username']} {amount} {item}s ==="
+        )
+
+        if item in inv1 and inv1[item]["amount"] >= amount:
+            if item in inv2:
+                inv2[item]["amount"] += amount
+            else:
+                inv2[item] = inv1[item].copy()
+                inv2[item]["amount"] = amount
+
+            inv1[item]["amount"] -= amount
+            if inv1[item]["amount"] == 0:
+                del inv1[item]
+
+            print("Transaction successful!\n")
+            print("=== Updated Inventories ===")
+            print(f"{inv1['username']} {item}s: {inv1[item]['amount']}")
+            print(f"{inv2['username']} {item}s: {inv2[item]['amount']}")
+
+        else:
+            print("Transaction failed!")
+
+    def print_inventory_analytics(inventories: list[dict]) -> None:
+        print("=== Inventory Analytics ===")
+
+        max_value: tuple | None = None
+        max_items: tuple | None = None
+        rarest_items: list = []
+
+        for inventory in inventories:
+            value = 0
+            items = 0
+
+            for key, item in inventory.items():
+                if key == "username":
+                    continue
+                value += item["price"] * item["amount"]
+                items += item["amount"]
+                if item["rarity"] == "rare":
+                    rarest_items.append(key)
+
+            if max_value is None or value > max_value[1]:
+                max_value = (inventory["username"], value)
+            if max_items is None or items > max_items[1]:
+                max_items = (inventory["username"], items)
+
+        print(f"Most valuable player: {max_value[0]} ({max_value[1]} gold)")
+        print(f"Most items: {max_items[0]} ({max_items[1]} items)")
+        print("Rarest items:", ", ".join(rarest_items))
+
     alice = {
+        "username": "Alice",
         "sword": {
             "amount": 1,
             "price": 500,
@@ -20,28 +104,20 @@ if __name__ = "__main__":
         },
     }
 
-"""
-$> python3 ft_inventory_system.py
-=== Player Inventory System ===
+    bob = {
+        "username": "Bob",
+        "magic_ring": {
+            "amount": 1,
+            "price": 400,
+            "type": "accessory",
+            "rarity": "rare",
+        },
+    }
 
-=== Alice's Inventory ===
-sword (weapon, rare): 1x @ 500 gold each = 500 gold
-potion (consumable, common): 5x @ 50 gold each = 250 gold
-shield (armor, uncommon): 1x @ 200 gold each = 200 gold
-
-Inventory value: 950 gold
-Item count: 7 items
-Categories: weapon(1), consumable(5), armor(1)
-
-=== Transaction: Alice gives Bob 2 potions ===
-Transaction successful!
-
-=== Updated Inventories ===
-Alice potions: 3
-Bob potions: 2
-
-=== Inventory Analytics ===
-Most valuable player: Alice (850 gold)
-Most items: Alice (5 items)
-Rarest items: sword, magic_ring
-"""
+    print("=== Player Inventory System ===")
+    print("\n=== Alice's Inventory ===")
+    print_inventory(alice)
+    print()
+    trade_items(alice, bob, "potion", 2)
+    print()
+    print_inventory_analytics([alice, bob])
